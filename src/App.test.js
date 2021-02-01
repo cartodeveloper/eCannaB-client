792731016bf6bd3d59071ed32928f86297a9e7f7
María Torrente <caritotorrente@gmail.com>
2021-02-01 10:42:54 -0500

    preparing for first deployment

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { BrowserRouter as Router } from "react-router-dom";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
