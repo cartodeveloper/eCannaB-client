8c0b0306da2dccc217403d5ec51ee66e02c06470
María Torrente <caritotorrente@gmail.com>
2021-02-09 19:45:42 -0500

    adding token service in header for logout, to clear it and push person out to /landing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import tokenService from "../services/token-service";
+import Context from "../Context";
 
 export default class Header extends React.Component {
+  static contextType = Context;
+  logout = (e) => {
+    e.preventDefault();
+    tokenService.clearAuthToken();
+    this.context.logout();
+    this.props.history.push("/");
+  };
   state = {
     opened: false,
   };
@@ -18,6 +26,17 @@ export default class Header extends React.Component {
         </section>
         {tokenService.hasAuthToken() ? (
           <>
+            <section className="user">
+              <Link
+                to="/logout"
+                className="logout"
+                aria-label="logout"
+                type="submit"
+                onClick={(e) => this.logout(e)}
+              >
+                Logout
+              </Link>
+            </section>
             <nav onClick={() => this.setState({ opened: !this.state.opened })}>
               <div className="nav-icon">
                 <h3>MENU</h3>
